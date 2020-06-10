ec4a032b57ca0a00b7cfa57b05d5467e4f54d3ee
Trần Minh Quý <ronalquy99@gmail.com>
2020-06-11 01:09:03 +0700

    like

--- storageGif.js
+++
@@ -1,9 +0,0 @@
-function saveGif() {
-  let values= [],
-    keys = Object.keys(localStorage),
-    for (let index = 0; index < keys.length; index++) {
-      values.push(localStorage.getItem(keys[i])); 
-    }
-  return values;
-}
-module.exports = { saveGif };
