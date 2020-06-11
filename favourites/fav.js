3c35f9241d60b4caf962c2de324ae79353c21edd
Trần Minh Quý <ronalquy99@gmail.com>
2020-06-11 22:38:53 +0700

    git add .

--- favourites/fav.js
+++ favourites/fav.js
@@ -2,7 +2,9 @@
 const { ipcRenderer } = require("electron");
 const displayGif = document.querySelector(".show");
 const urlLoad = "https://i.imgur.com/gVX3yPJ.gif";
+let lstDownload = [];
 
+//* Set favorites
 document.querySelector("#tab-favorites").innerText = `Favorites(${localStorage.length})`;
 //* add event for search
 document.querySelector("#tab-search").addEventListener("click", (e) => {
@@ -10,12 +12,14 @@ document.querySelector("#tab-search").addEventListener("click", (e) => {
     ipcRenderer.send("show-search");
 
 });
-//* add event for search
+//* add event to open upload
 document.querySelector("#tab-upload").addEventListener("click", (e) => {
     e.preventDefault();
     ipcRenderer.send("show-upload");
 
 });
+
+//* load image fav
 function loadImg() {
     const lstUrl = Object.values(localStorage);
     for (let i = 0; i < lstUrl.length; i++) {
@@ -32,12 +36,14 @@ function loadImg() {
         divImgCard.appendChild(img);
         img.style.display = 'none';
 
+        //* create img fav
         const favou = document.createElement('img');
         favou.classList.add('imgFav');
         favou.src = '../unlike.png';
 
         divImgCard.appendChild(favou);
 
+        //*add event for div 'cover img'
         divImgCard.onmouseover = function() {
             favou.style.display = 'block';
         }
@@ -45,20 +51,29 @@ function loadImg() {
             favou.style.display = 'none';
         }
 
+        //* check img loaded?
         img.onload = () => {
             divImgCard.style.backgroundImage = `url('${lstUrl[i]}')`;
-        }
-
+        };
+        //* add img like to list
         favou.addEventListener('click', (e) => {
             e.preventDefault();
             localStorage.removeItem(lstUrl[i]);
             displayGif.removeChild(divImgCard);
             document.querySelector("#tab-favorites").innerText = `Favorites(${localStorage.length})`;
         });
+
+        //*add url image to lstDownload
+        lstDownload.push(lstUrl[i]);
         displayGif.appendChild(divImgCard);
 
     }
 }
-
 loadImg();
+//* add event for btn export 'download'
+document.querySelector('#btnDownload').addEventListener('click', async(e) => {
+    e.preventDefault();
+    ipcRenderer.send('downloadFav', lstDownload);
+
+})
 console.log(localStorage);
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const { fetchGifSearch, fetchGifs } = require("./app.js");
 const fs = require("fs-extra");
 const Axios = require("axios");
+const pLimit = require("p-limit");
 const https = require("https");
 const path = require("path");
 const UPLOAD_ROOT = "https://upload.giphy.com/v1/gifs";
@@ -21,8 +22,8 @@ const createWindow = () => {
     mainWindow.loadFile("./search/search.html");
 
     //* fetch more
-    ipcMain.on("search-gifs", async(event, arg) => {
-        const keyWord = await fetchGifSearch(arg);
+    ipcMain.on("search-gifs", async(event, keyWordSearch) => {
+        const keyWord = await fetchGifSearch(keyWordSearch);
         event.reply("rely-search", keyWord);
     });
 
@@ -40,16 +41,50 @@ const createWindow = () => {
     ipcMain.on('show-upload', async(event, arg) => {
         mainWindow.loadFile('./upload/upload.html');
     })
-    
+
     //* open file to upload
     ipcMain.on('show-openFile', (event, arg) => {
-      dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] })
-        .then(path => {
-          if(path){
-            post(path);
-          }
-        }).catch(err => console.log(err));
+        dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] })
+            .then(path => {
+                if (path) {
+                    post(path);
+                }
+            }).catch(err => console.log(err));
+    });
+    //* download img
+    ipcMain.on('downloadFav', (event, urlImg) => {
+        dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] })
+            .then(async path => {
+                if (!path.canceled) {
+                    const limit = pLimit(5);
+                    const imgPromise = urlImg.map((url) => {
+                        return limit(() => {
+                            return downloadImage(url, `${path.filePaths[0]}.gif`);
+                        })
+                    })
+                    console.log(imgPromise);
+                    await Promise.all(imgPromise);
+                    dialog.showMessageBox({ message: "Download successful!!!" });
+                } else {
+                    throw new Error("open folder to export ...!");
+                }
+            }).catch(err => dialog.showMessageBox({ message: "download fail!" }))
     })
+
+    //* function download img
+    async function downloadImage(url, path) {
+        const respose = await Axios({
+            url,
+            method: 'GET',
+            resposeType: "stream"
+        })
+        const ws = fs.createWriteStream(path);
+        return new Promise((resolve, reject) => {
+            ws.once("close", resolve);
+            ws.once("error", reject);
+            respose.data.pipe(ws);
+        })
+    }
 };
 app.whenReady().then(() => {
     createWindow();
